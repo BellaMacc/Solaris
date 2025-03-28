54cbec5fc9f86c555c2f525439e4c3a8b4d4f04b
BellaMacc <meimei.macculloch@gmail.com>
2025-03-28 14:09:33 -0400

    fixing

--- src/components/mat-user-device.js
+++ src/components/mat-user-device.js
@@ -1,19 +1,21 @@
-
+'use strict';
 AFRAME.registerComponent("mat-user-device", {
     init: function(){
+        setTimeout('10000');
+        console.log("Solaris device registration component is registering")
         let textEntity = document.getElementById("displayText");
         if (AFRAME.utils.device.isMobile()) {
-            window.matPlayer.system.Portable = true;
+            //window.matPlayer.system.Portable = true;
             textEntity.setAttribute("value", "user Portable");
             console.log("user Portable");
         }
         else if (AFRAME.utils.device.checkHeadsetConnected()) {
-            window.matPlayer.system.Headset = true;
+           // window.matPlayer.system.Headset = true;
             textEntity.setAttribute("value", "user VR");
             console.log("user VR");
         }
         else{
-            window.matPlayer.system.Desktop = true;
+           // window.matPlayer.system.Desktop = true;
             textEntity.setAttribute("value", "user Computer");
             console.log("user Computer");
         }
