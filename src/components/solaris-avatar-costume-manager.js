0515cf35ed8cf3b7cf042304b6dcef95a8f3a362
tomato guy <119619910+catetack@users.noreply.github.com>
2025-04-02 09:57:17 -0400

    headmodel-swap changes
    currently all we have to do now is apply smiley texture to the head gltfs

--- src/components/solaris-avatar-costume-manager.js
+++ src/components/solaris-avatar-costume-manager.js
@@ -1,4 +1,7 @@
 'use strict';
+//what we need here is a socket.io shout function that happens whenever a player joins,
+// in this instance every player shouts their head information
+//following the mass shouting, we listen for all of the shouts
 
 AFRAME.registerComponent('avatar-costume-manager', {
     schema: {
@@ -15,6 +18,7 @@ AFRAME.registerComponent('avatar-costume-manager', {
         }
         
         CONTEXT_AF.el.sceneEl.addEventListener('changeCostume', function(event){
+            console.log("head Change here");
             CONTEXT_AF.receiveChangeRequest(event.detail.itemType);
         });
         
@@ -23,44 +27,65 @@ AFRAME.registerComponent('avatar-costume-manager', {
         
     },
     changeHead: function(itemType){
-        const CONTEXT_AF = this;
-        const avatar        = document.querySelector('#' + CIRCLES.CONSTANTS.PRIMARY_USER_ID);
-        const avatarNode    = avatar.querySelector('.user_' + 'head');
-        const data = CONTEXT_AF.data;
-
         
+        const CONTEXT_AF = this;
+        const avatar        = document.querySelector('#' + CIRCLES.CONSTANTS.PRIMARY_USER_ID);//gets this player's components 
+        const avatarNode    = avatar.querySelector('.user_' + 'head');//using "component search" we select the head component & everything underneath it
+        const avatarFace    = avatarNode.querySelector('.user_' + 'face_express');//narrows component down to face (smiley image layer)
+        
+        const data = CONTEXT_AF.data;
+        
+        console.log(CIRCLES.SOLARIS_HEADS['frog']);//array of all of our heads
+        console.log(avatarNode);
+        console.log(avatarFace);
+
 
         console.log("Head Changing Received");
 
+        //sets new head to default
         let newHeadSrc = CIRCLES.DEFAULT_GLTF_HEAD;
         let model = 0;
-        if (itemType === "frog"){
+        if (itemType === "frog"){//if item in the crypod is frog
             newHeadSrc = CIRCLES.SOLARIS_HEADS['frog'];
             avatarNode.setAttribute("circles-color", {color:'white'});
+            avatarFace.setAttribute("visible", "false")//makes face invisible by making the plane that the texture lies on invisible
+            avatarFace.setAttribute("material", { src: CIRCLES.CONSTANTS.SOLARIS_FACE_NONE})//makes face invisible by changing face texture
+            //if there would be an emit it would go here
 
           
            model = 5;
 
         }
-        else if (itemType === "chicken"){
+        else if (itemType === "chicken"){//if item in the crypod is frog
             newHeadSrc = CIRCLES.SOLARIS_HEADS['chicken'];
             avatarNode.setAttribute("circles-color", {color:'white'});
+            avatarFace.setAttribute("visible", "false")
+            avatarFace.setAttribute("material", { src: CIRCLES.CONSTANTS.SOLARIS_FACE_NONE})//makes face invisible
+            //here aswell
 
-           
             
             model = 6;
         }
         else{
             console.log("Head did not change item type not found");
         }
-        avatarNode.setAttribute("gltf-model", newHeadSrc);
+        avatarNode.setAttribute("gltf-model", newHeadSrc);//if a new head was chosen then a change is made to the gltf pointer
+
+        //Only useful for when player joins and leaves
         if (window.newURLSearchParams.has('head')){
-            window.newURLSearchParams.set('head', model);
-            window.newURLSearchParams.set('head'+ '_col',{color:'white'});
+            window.newURLSearchParams.set('head', model);//head=model#
+            window.newURLSearchParams.set('head'+ '_col',{color:'white'});//head_col
+
+            window.newURLSearchParams.set('face', "invisible");//
+
+
         }
         else{
+
             window.newURLSearchParams.append('head', model);
             window.newURLSearchParams.append('head'+ '_col',{color:'white'});
+            window.newURLSearchParams.append('face', "invisible");//
+
         }
 
     },
